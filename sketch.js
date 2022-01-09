d9c405a982fcefd287049e7b9b21c3631a4a252d
Progdruid <progdruid@gmail.com>
2022-01-09 15:43:01 +0200

    Sketch update 5

--- sketch.js
+++ sketch.js
@@ -8,6 +8,6 @@ function draw() {
     fill('red');
     beginShape();
     vertex(20, 20);
-    bezierVertex(100, 20, 100, 100, 20, 100);
+    bezierVertex(100, 20, 100, 100, 20, 20);
     endShape()
 }
